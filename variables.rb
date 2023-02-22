226c682a8b4d9432f9bec3609a56c0db56bca292
William Zale <williamzale@gmail.com>
2023-02-22 15:56:51 -0500

    Add string variables (previous variables were floats not strings)

--- variables.rb
+++ variables.rb
@@ -9,6 +9,8 @@ age = 35.6
 student_loan_balance = 1000000.00
 change_in_center_console = 0.75
 
-
 turing_mod_0 = "excellent"
+time_spent_coding = "worthwhile"
+learning_new_things = "enjoyable"
+
 days_learning_ruby = 1
