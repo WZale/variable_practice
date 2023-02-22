55408337b7b16a6e33b00b2776cee3226f74e1b1
William Zale <williamzale@gmail.com>
2023-02-22 16:18:36 -0500

    Reassign strings, add put statements and comments

--- variables.rb
+++ variables.rb
@@ -1,18 +1,36 @@
+# boolean
 student_loans = true
 married = false
 in_relationship = true
 happy = true
 
+puts happy
 
+# floats
 bank_balance = 1.01
 age = 35.6
 student_loan_balance = 1000000.00
 change_in_center_console = 0.75
 
+puts age
+
+# strings
 turing_mod_0 = "excellent"
 time_spent_coding = "worthwhile"
 learning_new_things = "enjoyable"
 
+puts turing_mod_0
+
+# strings reassigned
+turing_mod_0 = "educational"
+time_spent_coding = "a good investment"
+learning_new_things = "rewarding"
+
+puts turing_mod_0
+
+# integers
 days_learning_ruby = 1
 cats = 1
 pens_on_desk = 12
+
+puts cats
